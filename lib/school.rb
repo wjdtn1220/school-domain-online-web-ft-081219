29c2c258345f595f50ac9349a2a852a82c8be63d
Jeong Park <wjdtn1220@gmail.com>
2019-08-27 18:07:25 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,26 +1,29 @@
 class School
-  attr_accessor :name, :roster
+  attr_reader :name, :roster
   
   def initialize(name)
     @name = name
     @roster = {}
   end
   
-  def add_student(name, students_grade)
-    if roster.include?(students_grade) == false
-      @roster[students_grade] = []
+  def add_student(students_name, grade)
+    if roster[grade]
+      roster[grade] << students_name
+    else
+      roster[grade] = []
+      roster[grade] << students_name
     end
-    @roster[students_grade] << name 
   end
   
-  def grade(students_level)
-    @roster[students_level]
+  def grade(grade)
+    @roster[grade]
   end
   
   def sort
-    @roster.each do |students_grade, name|
-      @roster[students_grade] = name.sort
+    new_grades = {}
+    @roster.each do |grade, students_name|
+      new_grades[grade] = students_name.sort
     end
+    new_grades
   end
 end
-
